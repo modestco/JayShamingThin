deb75a228caefe8e095c3d021ce89652c1594c91
Modestly Jen <118849300+modestco@users.noreply.github.com>
2023-08-13 09:55:37 -0400

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { initThinBackend } from 'thin-backend';
+import { ThinBackend } from 'thin-backend-react';
+import 'thin-backend-react/auth.css';
+
+initThinBackend({ host: process.env.NEXT_PUBLIC_BACKEND_URL });
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <ThinBackend requireLogin><Component {...pageProps} /></ThinBackend>
+}
+export default MyApp
